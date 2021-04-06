70e468bb5da338ff356555f73d7221b39ddd065c
Nathan Banks <nathan@rs21.io>
2021-04-06 14:36:41 -0600

    first round

--- app.py
+++ app.py
@@ -12,6 +12,19 @@ def load_image(path):
     with open(path,"rb") as img:
         return base64.b64encode(img.read()).decode()
 
+def get_label(text, section):
+    """
+    Adds a class to labels to allow custom coloring based on the section.
+    text (str): label to be displayed
+    section (str): one of "main" or "sidebar"
+    ex: get_label("label", "main")
+    """
+    if section == "main":
+        return f'<p class=\'main-label\'>{text}</p>';
+    elif section == "sidebar":
+        return f'<p class=\'sidebar-label\'>{text}</p>';
+
+
 def download_link(object_to_download, download_filename, download_link_text):
     """
     Generates a link to download the given object_to_download.
@@ -33,12 +46,25 @@ icon_url = f"data:image/png;base64,{load_image('./assets/static/logo.png')}"
 title_string = 'RS21 DS Prototype'
 
 top_logo_html=f"""
-    <div style="z-index: 101; display:inline-block; position: fixed; left: 0; top: 0;">
-        <img style="padding-left:15px; padding-right: 10px; padding-bottom: 8px; display:inline-block;" src={icon_url}>
-        <h2 style="display: inline-block;">{title_string}</h2>
+    <div class="topdiv">
+        <img class="toplogo" src={icon_url}>
+        <div class="toptext">{title_string}</div>
     </div>
     """
 
 st.set_page_config(layout="wide", page_title=title_string, page_icon=icon_url)
 local_css('./assets/css/main.css')
 st.markdown(top_logo_html, unsafe_allow_html=True)
+st.sidebar.header("hey")
+st.markdown("<h2 class='title'>hey again</h2>",unsafe_allow_html=True)
+
+st.sidebar.markdown(get_label('first label', 'sidebar'),unsafe_allow_html=True)
+option = st.sidebar.selectbox('', ('Something 1', 'Something 2'))
+
+
+st.sidebar.markdown(get_label('second label', 'sidebar'),unsafe_allow_html=True)
+options = st.sidebar.multiselect('', ['green','yellow', 'blue', 'red'],['yellow','blue'])
+
+st.markdown(get_label('main label', 'main'),unsafe_allow_html=True)
+option_main = st.selectbox('', ('something', 'something else'))
+
