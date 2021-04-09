bd8d545ffc037300897ece73c4e04a47e0b818c8
Nathan Banks <nathan@rs21.io>
2021-04-08 18:08:55 -0600

    better style

--- app.py
+++ app.py
@@ -12,19 +12,6 @@ def load_image(path):
     with open(path,"rb") as img:
         return base64.b64encode(img.read()).decode()
 
-def get_label(text, section):
-    """
-    Adds a class to labels to allow custom coloring based on the section.
-    text (str): label to be displayed
-    section (str): one of "main" or "sidebar"
-    ex: get_label("label", "main")
-    """
-    if section == "main":
-        return f'<p class=\'main-label\'>{text}</p>';
-    elif section == "sidebar":
-        return f'<p class=\'sidebar-label\'>{text}</p>';
-
-
 def download_link(object_to_download, download_filename, download_link_text):
     """
     Generates a link to download the given object_to_download.
@@ -44,6 +31,7 @@ def download_link(object_to_download, download_filename, download_link_text):
 icon_url = f"data:image/png;base64,{load_image('./assets/static/logo.png')}"
 
 title_string = 'RS21 DS Prototype'
+#title_string = ''
 
 top_logo_html=f"""
     <div class="topdiv">
@@ -53,18 +41,29 @@ top_logo_html=f"""
     """
 
 st.set_page_config(layout="wide", page_title=title_string, page_icon=icon_url)
+
 local_css('./assets/css/main.css')
-st.markdown(top_logo_html, unsafe_allow_html=True)
-st.sidebar.header("hey")
-st.markdown("<h2 class='title'>hey again</h2>",unsafe_allow_html=True)
+st.markdown("<link rel=\"stylesheet\" type=\"text/css\" href=\"//fonts.googleapis.com/css?family=Open+Sans\" />", unsafe_allow_html=True)
 
-st.sidebar.markdown(get_label('first label', 'sidebar'),unsafe_allow_html=True)
-option = st.sidebar.selectbox('', ('Something 1', 'Something 2'))
+sidebar, main = st.beta_columns((1,4))
 
+with sidebar:
+    st.markdown(top_logo_html, unsafe_allow_html=True)
+    st.markdown("<p class='section-title'>Title</p>",unsafe_allow_html=True)
 
-st.sidebar.markdown(get_label('second label', 'sidebar'),unsafe_allow_html=True)
-options = st.sidebar.multiselect('', ['green','yellow', 'blue', 'red'],['yellow','blue'])
+    text_side = st.text_input('Text Input', 'Placeholder')
 
-st.markdown(get_label('main label', 'main'),unsafe_allow_html=True)
-option_main = st.selectbox('', ('something', 'something else'))
+    select_side = st.selectbox('Dropdown Select', ('Option 1', 'Option 2','Option 3'))
+    test_sel = st.radio('Radio Button', ('Opt 1', 'Opt 2')) 
 
+with main:
+    st.markdown(f"<p class='title'>{title_string}</p>",unsafe_allow_html=True)
+    
+    expander1 = st.beta_expander("Panel title", expanded=False)
+    with expander1:
+        st.markdown("<p class='content2'>Other content</p>",unsafe_allow_html=True)
+    expander2 = st.beta_expander("Panel title", expanded=True)
+    with expander2:
+        st.markdown("<p class='content2'>The content within an accordion panel could include a variety of content types. The content can be – a long paragraph of description text, structured content, unordered lists, images with captions, simple or complex tables, data visualizations and a lot more.The content within an accordion panel could include a variety of content types. The content can be – a long paragraph of description text, structured content, unordered lists, images with captions, simple or complex tables, data visualizations and a lot more.</p>", unsafe_allow_html=True);
+
+    st.markdown("<div class='card'><p class='card-title'>Warning</p><p class='content2'>Your selected area exceeds lorem ipsum dolor sit amet, consectetur adipiscing elit.</p><p class='content2'>Cras tincidunt sit amet nibh ut imperdiet. Suspendisse dictum finibus velit, in ullamcorper nibh efficitur non. Pellentesque aliquet quam in lorem viverra, et condimentum nunc fringilla. Morbi non tempus leo. Curabitur eget velit risus. Quisque dictum risus ut mattis semper. Ut semper nulla luctus, rutrum elit a, iaculis neque. Integer malesuada sollicitudin risus, sed pulvinar justo volutpat pretium. Sed fermentum scelerisque diam fermentum congue. Maecenas non est ante. Proin eu ex ante.</p></div>",unsafe_allow_html=True);
