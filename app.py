f35bd41b5b8462e69bbed7e6da1782cf187c58ff
Nathan Banks <nathan@rs21.io>
2021-04-21 11:11:45 -0600

    removing theme file because it's inconsistent

--- app.py
+++ app.py
@@ -1,9 +1,94 @@
 import streamlit as st
 import streamlit.components.v1 as components
-import theme
+import base64
 import pandas as pd
 import numpy as np
+import argparse
+import sys
 
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
+
+def load_image(path):
+    with open(path,"rb") as img:
+        return base64.b64encode(img.read()).decode()
+
+def download_link(object_to_download, download_filename, download_link_text, align="left"):
+    """
+    Generates a link to download the given object_to_download.
+    object_to_download (str, pd.DataFrame):  The object to be downloaded.
+    download_filename (str): filename and extension of file. e.g. mydata.csv, some_txt_output.txt
+    download_link_text (str): Text to display for download link.
+    align (str): optional "right" or "left" argument to align the button on the right. left is default
+    Examples:
+        download_link(YOUR_DF, 'YOUR_DF.csv', 'Click here to download data!')
+        download_link(YOUR_STRING, 'YOUR_STRING.txt', 'Click here to download your text!')
+    """
+    if isinstance(object_to_download,pd.DataFrame):
+        object_to_download = object_to_download.to_csv(index=False)
+        # some strings <-> bytes conversions necessary here
+        b64 = base64.b64encode(object_to_download.encode()).decode()
+        return f'<a class="downloadlink f{align}" href="data:file/txt;base64,{b64}" download="{download_filename}">{download_link_text}</a>'
+
+def get_card(title, content, align="left"):
+    """
+    Generate the html to build a "card".
+    title (str): the title of the card
+    content (str): the content of the card
+    align (str): optional "right" or "left" argument to align card on the right. left is default
+
+    """
+    return f"<div class='card  f{align}'><h2 class=card-title>{title}</h2><p class='card'>{content}</p></div>"
+
+def get_accordion(content):
+    return f"<p class='card'>{content}</p>"
+
+def parse_args(args):
+    parser = argparse.ArgumentParser('DS POC')
+    parser.add_argument('--theme', help='Theme: dark or light', default='dark', required=False)
+    return parser.parse_args(args)
+
+args = parse_args(sys.argv[1:])
+cust_theme = args.theme
+
+# load in .css file/logo and write to the config.toml logo based on theme choice
+
+if cust_theme == "light":
+    icon_url = f"data:image/png;base64,{load_image('./assets/static/logo-light.png')}"
+    css_file = ('./assets/css/light.css')
+    f = open("./.streamlit/config.toml", "w")
+    toml_txt = "[theme]\nprimaryColor='#908F8F'\nbackgroundColor='#E8E8F0'\nsecondaryBackgroundColor='#F8F8F8'\ntextColor='#23242B'\nfont='sans serif'"
+    f.write(toml_txt)
+    f.close()
+else:
+    icon_url = f"data:image/png;base64,{load_image('./assets/static/logo-dark.png')}"
+    css_file = ('./assets/css/dark.css')
+    f = open("./.streamlit/config.toml", "w")
+    toml_txt = "[theme]\nprimaryColor='#908F8F'\nbackgroundColor='#0e1117'\nsecondaryBackgroundColor='#23242B'\ntextColor='#fafafa'\nfont='sans serif'"
+    f.write(toml_txt)
+    f.close()
+
+# sets widescreen and the page_icon and title that show up in the tab
+
+title_string = 'RS21 DS Prototype'
+st.set_page_config(layout="wide", page_title=title_string, page_icon=icon_url)
+
+local_css(css_file)
+local_css('./assets/css/main.css')
+
+# Open Sans font
+st.markdown("<link rel=\"stylesheet\" type=\"text/css\" href=\"//fonts.googleapis.com/css?family=Open+Sans\" />", unsafe_allow_html=True)
+
+top_logo_html=f"""
+    <div class="topdiv">
+        <img class="toplogo" src={icon_url}>
+        <div class="toptext">{title_string}</div>
+    </div>
+    """
+
+# writes the logo and title into the header
+st.markdown(top_logo_html, unsafe_allow_html=True)  
 
 ## EVERYTHING FROM HERE DOWN IS JUST EXAMPLES!
 
@@ -14,7 +99,7 @@ chart_data = pd.DataFrame(
      columns=['a', 'b', 'c'])
 
 df = pd.read_csv('./assets/data/stormofswords.csv')
-dl_link = theme.download_link(df, "test.csv", "Download", "right")
+dl_link = download_link(df, "test.csv", "Download", "right")
 
 with sidebar:
     st.header("Data Inputs")
@@ -29,16 +114,16 @@ with sidebar:
 
 
 with main:
-    st.title(theme.title_string)
+    st.title(title_string)
     
     expander1 = st.beta_expander("Accordion title", expanded=False)
     with expander1:
-        st.markdown(theme.get_accordion("Hidden accordion content"),unsafe_allow_html=True)
+        st.markdown(get_accordion("Hidden accordion content"),unsafe_allow_html=True)
     expander2 = st.beta_expander("Accordion title", expanded=True)
     with expander2:
         st.markdown("<p class='card'>The content within an accordion panel could include a variety of content types. The content can be – a long paragraph of description text, structured content, unordered lists, images with captions, simple or complex tables, data visualizations and a lot more.The content within an accordion panel could include a variety of content types. The content can be – a long paragraph of description text, structured content, unordered lists, images with captions, simple or complex tables, data visualizations and a lot more.</p>", unsafe_allow_html=True)
 
-    st.markdown(theme.get_card("Cool card title","Lots of card content and stuff<br><br>tincidunt sit amet nibh ut imperdiet. Suspendisse dictum finibus velit, in ullamcorper nibh efficitur non. Pellentesque aliquet quam in lorem viverra, et condimentum nunc fringilla. Morbi non tempus leo. Curabitur eget velit risus. Quisque dictum risus ut mattis semper. Ut semper nulla luctus, rutrum elit a, iaculis neque. Integer malesuada sollicitudin risus, sed pulvinar justo volutpat pretium. Sed fermentum scelerisque diam fermentum congue. Maecenas non est ante. Proin eu ex ante.","right"),unsafe_allow_html=True)
+    st.markdown(get_card("Cool card title","Lots of card content and stuff<br><br>tincidunt sit amet nibh ut imperdiet. Suspendisse dictum finibus velit, in ullamcorper nibh efficitur non. Pellentesque aliquet quam in lorem viverra, et condimentum nunc fringilla. Morbi non tempus leo. Curabitur eget velit risus. Quisque dictum risus ut mattis semper. Ut semper nulla luctus, rutrum elit a, iaculis neque. Integer malesuada sollicitudin risus, sed pulvinar justo volutpat pretium. Sed fermentum scelerisque diam fermentum congue. Maecenas non est ante. Proin eu ex ante.","right"),unsafe_allow_html=True)
 
     if test_sel == 'Yes':
         st.dataframe(df)
--- readme.md
+++ readme.md
@@ -3,9 +3,8 @@
 > pip install streamlit
 
 ## Running
-Adding "import theme" to the top of the streamlit python file will bring in all of the styles and functions needed. It will style all of the imports and text without any changes to the regular streamlit calls. An app could also be built using theme.py as the starting point.\
-\
-There are two example versions of theme.py being used.
+There are two example versions of a streamlit app using the theme.\
+The first 90 or so lines are necessary for the theme, and the rest is just examples of using the components.
 
 ### app.py 
 Set up with inputs on a left "sidebar" and the data displays/text in a main section.
@@ -15,7 +14,7 @@ Set up with inputs on a left "sidebar" and the data displays/text in a main sect
 Set up with inputs at the top and the data displays/text below.
 > streamlit run app-no-sidebar.py
 
-Anything using the theme.py file can accept a theme argument with either "light" or "dark" as options:
+Both can accept a theme argument with either "light" or "dark" as options:
 > streamlit run app.py -- --theme light
 
 dark is the default\
@@ -43,20 +42,20 @@ there is a "get_accordion()" function that takes a string and returns the string
 ```
 expander1 = st.beta_expander("Accordion title", expanded=False)
 with expander1:
-    st.markdown(theme.get_accordion("Hidden accordion content"),unsafe_allow_html=True)
+    st.markdown(get_accordion("Hidden accordion content"),unsafe_allow_html=True)
 ```
 
 ### Cards:
 There is a "get_card()" function that takes two args (title, content) and wraps them in the necessary html tags and returns the full html needed for the card.\
 get_card() has a third optional argument "align" that allows you to pass the string "right" to align the card on the right side of the screen.
 ```
-st.markdown(theme.get_card("title","content"), unsafe_allow_html=True)
+st.markdown(get_card("title","content"), unsafe_allow_html=True)
 ```
 
 ### Download link:
-There is a download_link() function in the theme file that returns the html for a download button.\
+There is a download_link() function that returns the html for a download button.\
 download_link has the same optional argument "align" that allows you to align the button on the right side of the screen.
 ```
-download_html = theme.download_link(df, filename, 'EXPORT DATA')
+download_html = download_link(df, filename, 'EXPORT DATA')
 st.markdown(download_html, unsafe_allow_html=True)
 ```
