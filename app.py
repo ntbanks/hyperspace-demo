74d0f0576dd625322cf3217f3b63ae03e966b9d1
David Dooling <davidd@Corp.rs21.io>
2021-04-22 10:49:54 -0600

    edited app.py

--- app.py
+++ app.py
@@ -5,6 +5,17 @@ import pandas as pd
 import numpy as np
 import argparse
 import sys
+import matplotlib.pyplot as plt
+import sqlite3
+import pickle
+from pickle import load
+from keras.models import load_model
+import keras
+from sklearn.preprocessing import StandardScaler
+
+from time import sleep
+
+
 
 def local_css(file_name):
     with open(file_name) as f:
@@ -90,54 +101,112 @@ top_logo_html=f"""
 # writes the logo and title into the header
 st.markdown(top_logo_html, unsafe_allow_html=True)  
 
+
+
+
+@st.cache
+def get_dfval_frame():
+    with sqlite3.connect('turbofandata.db') as connection:
+        dfval_new = pd.read_sql('SELECT * from dfval_notime', connection)
+        return dfval_new
+    
+@st.cache
+def load_saved_model():
+    model = load_model('model_notime.h5')
+    
+    
+@st.cache
+def load_scaler():
+    scaler_xtrain = load(open('scaler_xtrain_notime.pkl','rb'))
+    
+
+
+def get_single_hazard(params:dict,
+                     scaler_xtrain, clf):
+    """takes a single sample of data in the form of a dictionary and 
+    returns the hazard function predicted by the model clf."""
+    db = pd.DataFrame(params.values(), index=b.keys()).T[input_cols]
+    X = scaler_xtrain.transform(db.to_numpy())
+    hazard = clf.predict(X)
+    survival_curve.append(survival_curve[-1]*(1 - hazard[0][0]))
+    return hazard
+
+
+
+
+    
+dfval = get_dfval_frame()
+#model = load_saved_model()
+model = load_model('model_notime.h5')
+#scaler_xtrain = load_scaler()
+scaler_xtrain = load(open('scaler_xtrain_notime.pkl','rb'))
+#st.write(f'{type(model)} : {type(dfval)} : {type(scaler_xtrain)}')
+input_cols = ['setting_1', 'setting_2', 'setting_3', 's_1', 
+              's_2', 's_3', 's_4', 's_5', 's_6', 's_7', 's_8',
+              's_9', 's_10', 's_11', 's_12', 's_13', 's_14',
+              's_15', 's_16', 's_17', 's_18', 's_19', 's_20', 's_21']
+
+
+
+sats = tuple(dfval['id'].unique())
+sat = st.sidebar.selectbox('Select Satellite', sats)
+
+
+dfsat = dfval[dfval['id']==sat]
+dfsat.reset_index(inplace=True)
+
+
+
+
+
 ## EVERYTHING FROM HERE DOWN IS JUST EXAMPLES!
 
-sidebar, main = st.beta_columns((1,4))
+#sidebar, main = st.beta_columns((1,4))
 
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
+#chart_data = pd.DataFrame(
+#     np.random.randn(20, 3),
+#     columns=['a', 'b', 'c'])
 
-df = pd.read_csv('./assets/data/stormofswords.csv')
-dl_link = download_link(df, "test.csv", "Download", "right")
+#df = pd.read_csv('./assets/data/stormofswords.csv')
+#dl_link = download_link(df, "test.csv", "Download", "right")
 
-with sidebar:
-    st.header("Data Inputs")
-    st.markdown("Regular content that's not in an accordion or card looks like this")
-    st.text("text text text")
-    text_side = st.text_input('Text Input', 'Placeholder')
+#with sidebar:
+#    st.header("Data Inputs")
+#    st.markdown("Regular content that's not in an accordion or card looks like this")
+#    st.text("text text text")
+#    text_side = st.text_input('Text Input', 'Placeholder')
 
-    select_side = st.selectbox('Dropdown Select', ('Option 1', 'Option 2','Option 3'))
-    test_sel = st.radio('Show Data', ('Yes', 'No')) 
-    check_test = st.checkbox('Show line chart')
-    st.markdown(dl_link, unsafe_allow_html=True)
+#    select_side = st.selectbox('Dropdown Select', ('Option 1', 'Option 2','Option 3'))
+#    test_sel = st.radio('Show Data', ('Yes', 'No')) 
+#    check_test = st.checkbox('Show line chart')
+#    st.markdown(dl_link, unsafe_allow_html=True)
 
 
-with main:
-    st.title(title_string)
+#with main:
+#    st.title(title_string)
     
-    expander1 = st.beta_expander("Accordion title", expanded=False)
-    with expander1:
-        st.markdown(get_accordion("Hidden accordion content"),unsafe_allow_html=True)
-    expander2 = st.beta_expander("Accordion title", expanded=True)
-    with expander2:
-        st.markdown("<p class='card'>The content within an accordion panel could include a variety of content types. The content can be – a long paragraph of description text, structured content, unordered lists, images with captions, simple or complex tables, data visualizations and a lot more.The content within an accordion panel could include a variety of content types. The content can be – a long paragraph of description text, structured content, unordered lists, images with captions, simple or complex tables, data visualizations and a lot more.</p>", unsafe_allow_html=True)
+#    expander1 = st.beta_expander("Accordion title", expanded=False)
+#    with expander1:
+#        st.markdown(get_accordion("Hidden accordion content"),unsafe_allow_html=True)
+#    expander2 = st.beta_expander("Accordion title", expanded=True)
+#    with expander2:
+#        st.markdown("<p class='card'>The content within an accordion panel could include a variety of content types. The content can be – a long paragraph of description text, structured content, unordered lists, images with captions, simple or complex tables, data visualizations and a lot more.The content within an accordion panel could include a variety of content types. The content can be – a long paragraph of description text, structured content, unordered lists, images with captions, simple or complex tables, data visualizations and a lot more.</p>", unsafe_allow_html=True)
 
-    st.markdown(get_card("Cool card title","Lots of card content and stuff<br><br>tincidunt sit amet nibh ut imperdiet. Suspendisse dictum finibus velit, in ullamcorper nibh efficitur non. Pellentesque aliquet quam in lorem viverra, et condimentum nunc fringilla. Morbi non tempus leo. Curabitur eget velit risus. Quisque dictum risus ut mattis semper. Ut semper nulla luctus, rutrum elit a, iaculis neque. Integer malesuada sollicitudin risus, sed pulvinar justo volutpat pretium. Sed fermentum scelerisque diam fermentum congue. Maecenas non est ante. Proin eu ex ante.","right"),unsafe_allow_html=True)
+#    st.markdown(get_card("Cool card title","Lots of card content and stuff<br><br>tincidunt sit amet nibh ut imperdiet. Suspendisse dictum finibus velit, in ullamcorper nibh efficitur non. Pellentesque aliquet quam in lorem viverra, et condimentum nunc fringilla. Morbi non tempus leo. Curabitur eget velit risus. Quisque dictum risus ut mattis semper. Ut semper nulla luctus, rutrum elit a, iaculis neque. Integer malesuada sollicitudin risus, sed pulvinar justo volutpat pretium. Sed fermentum scelerisque diam fermentum congue. Maecenas non est ante. Proin eu ex ante.","right"),unsafe_allow_html=True)
 
-    if test_sel == 'Yes':
-        st.dataframe(df)
+#    if test_sel == 'Yes':
+#        st.dataframe(df)
 
-    if check_test:
-        st.line_chart(chart_data)
+#    if check_test:
+#        st.line_chart(chart_data)
 
-    st.button('ST Button')
-    st.multiselect('Multiselect', [1,2,3])
-    st.slider('Slide me', min_value=0, max_value=10)
-    st.select_slider('Slide to select', options=[1,'2'])
-    st.number_input('Enter a number')
-    st.text_area('Area for textual entry')
-    st.date_input('Date input')
-    st.time_input('Time entry')
-    st.file_uploader('File uploader')
-    st.color_picker('Pick a color')
+#    st.button('ST Button')
+#    st.multiselect('Multiselect', [1,2,3])
+#    st.slider('Slide me', min_value=0, max_value=10)
+#    st.select_slider('Slide to select', options=[1,'2'])
+#    st.number_input('Enter a number')
+#    st.text_area('Area for textual entry')
+#    st.date_input('Date input')
+#    st.time_input('Time entry')
+#    st.file_uploader('File uploader')
+#    st.color_picker('Pick a color')
